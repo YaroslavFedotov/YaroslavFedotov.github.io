9e3f85cb4f893a4dd373f2c26b91515f4015186b
YaroslavFedotov <56696549+YaroslavFedotov@users.noreply.github.com>
2024-07-09 01:23:17 +0300

    Update main.js
    ?

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,6 @@
-jQuery(document).ready(function($) {
 
-
-    /*======= Skillset *=======*/
     
-    
-    $('.level-bar-inner').css('width', '0');
-    
-    $(window).on('load', function() {
-
-        $('.level-bar-inner').each(function() {
-        
-            var itemWidth = $(this).data('level');
-            
-            $(this).animate({
-                width: itemWidth
-            }, 800);
-            
-        });
-
-    });
+    <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A56cbeb5589789bc2c68d4c6ab309aacfb4fa897636151100ac5b320243596232&amp;width=600&amp;height=600&amp;lang=ru_RU&amp;scroll=true"></script>
    
     
 
-});
